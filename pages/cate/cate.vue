8ed8fdfa52a364f5d6a718b60e13975063775904
lb2281075105 <2281075105@qq.com>
2021-09-28 10:43:43 +0800

    cate left scroll-view

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,44 +1,23 @@
 <template>
 	<!-- 并不是小程序里面全部使用rpx,固定的宽度和高度使用px -->
   <view class="scroll-view-container">
-    <view class="left-scroll-view" :style="{height: wh + 'px';}">
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
+    <scroll-view class="left-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
 		
-	</view>
-	<view class="right-scroll-view" :style="{height: wh + 'px';}">
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		<view>xxx</view>
-		
-	</view>
+		<block v-for="(item,i) in cateList" :key="i">
+			<view :class="['left-scroll-view-item',i === active ? 'active':''] " @click="activeChange(i)">{{item.cat_name}}</view>
+		</block>
+	</scroll-view>
+	<scroll-view class="right-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
+		<view>yyy</view>
+		<view>yyy</view>
+		<view>yyy</view>
+		<view>yyy</view>
+		<view>yyy</view>
+		<view>yyy</view>
+		<view>yyy</view>
+		<view>yyy</view>
+	
+	</scroll-view>
   </view>
 </template>
 
@@ -47,13 +26,30 @@
   export default {
     data() {
       return {
-        wh:0
+        wh:0,
+		cateList:[],
+		active:0
       };
     },
 	onLoad() {
 		const systemInfo = uni.getSystemInfoSync()
-		console.log(systemInfo)
+		this.wh = systemInfo.windowHeight
+		this.getCateList()
+	},
+	methods:{
+		async getCateList(){
+			const {data:res} = await uni.$http.get("/api/public/v1/categories")
+			if(res.meta.status == 200){
+			  this.cateList = res.message
+			}else{
+			  uni.$showMsg()
+			}
+		},
+		activeChange(i){
+			this.active = i
+		}
 	}
+
   }
 </script>
 
@@ -62,6 +58,30 @@
 	display: flex;
 	.left-scroll-view{
 		width: 120px;
+		.left-scroll-view-item{
+			background-color: #F7F7F7;
+			line-height: 60px;
+			text-align: center;
+			font-size: 12px;
+			
+			&.active{
+				background-color: #FFFFFF;
+				position: relative;
+				&::before{
+					content: ' ';
+					position: absolute;
+					top: 50%;
+					left: 0;
+					transform: translateY(-50%);
+					background-color: #C00000;
+					display: block;
+					width: 3px;
+					height: 30px;
+				}		
+			}
+			
+		}
+
 	}
 }
 </style>
