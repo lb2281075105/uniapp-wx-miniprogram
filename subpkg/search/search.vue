0fe91b06ae568dafeffb60500d6448c88f67eb21
lb2281075105 <2281075105@qq.com>
2021-09-28 17:26:41 +0800

    持久化

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -5,12 +5,23 @@
 				<!-- <view slot="clearIcon" style="color: #999999" >X</view> -->
 			</uni-search-bar>
 		</view>
-		<view class="goods-list">
-			<view v-for="(item,i) in searchResults" class="goods-item">
+		<view class="goods-list" v-if="searchResults.length !== 0">
+			<view v-for="(item,i) in searchResults" class="goods-item" @click="gotoGoodsTail(item)" :key="i">
 				<view class="goods-name">
 					{{item.goods_name}}
 				</view>
 				<uni-icons type="arrowright" size="16"></uni-icons>
+			</view>
+		</view>
+		
+		<view class="history-box" v-else>
+				
+			<view class="history-title">
+				<view>搜索历史</view>
+				<uni-icons type="trash"></uni-icons>
+			</view>
+			<view class="history-list">
+				<uni-tag :text="item" v-for="(item,i) in histories" :key="i"></uni-tag>
 			</view>
 		</view>
 	</view>
@@ -23,8 +34,15 @@
 				timer:null,
 				searchValue:"",
 				kw:'',
-				searchResults:[]
+				searchResults:[], 
+				historyList:JSON.parse(uni.getStorageSync('kw') || '[]')
 			};
+		},
+		computed:{
+			// 计算属性处理搜索文字在最前面显示
+			histories(){
+				return [...this.historyList].reverse()
+			}
 		},
 		methods:{
 			input(e){
@@ -33,8 +51,17 @@
 					console.log(e)
 					this.kw = e
 					this.getSearchList()
+					this.saveHistoryKeyword()
 				},500)
 				
+			},
+			saveHistoryKeyword(){
+				// 处理搜索文字重复的问题
+				let set = new Set(this.historyList)
+				set.delete(this.kw)
+				set.add(this.kw)
+				this.historyList = Array.from(set)
+				uni.setStorageSync('kw',JSON.stringify(this.historyList))
 			},
 			async getSearchList(){
 				
@@ -49,6 +76,11 @@
 				}else{
 				  uni.$showMsg()
 				}
+			},
+			gotoGoodsTail(item){
+				uni.navigateTo({
+					url:'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+				})
 			}
 		}
 	}
@@ -75,6 +107,21 @@
 				font-size: 12px;
 				margin-right: 3px;
 			}
+		}
+	}
+	.history-box{
+		padding: 0 5px;
+		.history-title{
+			display: flex;
+			justify-content: space-between;
+			font-size: 12px;
+			border-bottom: 1px solid #efefef;
+			height: 40px;
+			align-items: center;
+		}
+		.uni-tag{
+			margin-top: 5px;
+			margin-right: 5px;
 		}
 	}
 	
